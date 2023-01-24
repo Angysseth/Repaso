d21d20ecd3398ebd56dd95fccf39800cea92e193
Angela Gysseth <angyssethc@hotmail.com>
2023-01-23 21:43:20 -0500

    repaso

---
+++ Basic/inicio.js
@@ -0,0 +1,14 @@
+let martinez = "Canson"
+const willi = "Re canson"
+
+console.log(martinez);
+
+
+const david = {
+    ojos: "bonitos",
+    sonrisa: "encantadora",
+    edad: 1000,
+    meEncanta: true
+}
+
+console.log(david.sonrisa);
