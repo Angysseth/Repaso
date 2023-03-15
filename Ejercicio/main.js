33912af0a06155d9ac5b6313a53af72b4b5e9a94
Angela Gysseth <angyssethc@hotmail.com>
2023-03-14 21:29:23 -0500

    Ejercicio

--- Ejercicio/main.js
+++ Ejercicio/main.js
@@ -1,10 +1,20 @@
+const arregloTodo = []
+
 const main = () => {
     const formulario = document.getElementById("todo-list")
-    formulario.onsubmit(e) =>{
-    e.preventDefault()
-    const campo = document.getElementById("campoTexto")
-    campoTexto = campo.value
-    console.log(campoTexto);
-    campoTexto.value = " "
+    formulario.onsubmit = (e) => {
+        e.preventDefault()
+        const campo = document.getElementById("campoTexto")
+        campoTexto = campo.value
+        console.log(campoTexto)
+        campo.value = " "
+        arregloTodo.push(campoTexto)
+        const elementoLista = document.getElementById("list-element")
+        elementoLista.innerText = " "
+
+        for (let index = 0; index < arregloTodo.length; index++) {
+            elementoLista.innerHTML += "<li>" + arregloTodo[index]+ "</li>"
+            
+        }
     }
 }
